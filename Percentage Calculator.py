db633b2792dbf45e211ab2ffaa3acd41546a1414
Aron <62036603+Aronace21@users.noreply.github.com>
2020-03-27 15:53:54 -0400

    Update Percentage Calculator.py

--- Percentage Calculator.py
+++ Percentage Calculator.py
@@ -1,7 +1,7 @@
 print("Hello what percentage would you like for me to calculate?")
-print("make your number.")
-myNumbeqr = input("What number do you want to find the percent of: ")
+print("Put in the number that it asks you for.")
+myNumber = input("What number do you want to find the percent of: ")
 myPercent = input ("What is the percent you want me to find?: ")
-theAnswer = (int(myPercent)) * (int(myNumbeqr)) / 100
+theAnswer = (int(myPercent)) * (int(myNumber)) / 100
 print ("the answer is")
 print(theAnswer)
